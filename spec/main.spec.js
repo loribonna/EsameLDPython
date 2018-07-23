4250aa4b1a7302749b6362adcbfcd3ef7b11d600
Lorenzo Bonicelli <lorenzobonicelli@libero.it>
2018-07-24 01:14:45 +0200

    Fix db_dump.json incorrect starting byte

---
+++ spec/main.spec.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("jasmine");
+describe("Map", function () {
+    it("Should build tests", function () {
+        expect(true).toBe(true);
+    });
+});
